1c2a1650eb28090142efb30dfce493471b9e3e26
vikrant chauhan <i.vikrant.me@gmail.com>
2025-04-04 00:45:50 +0530

    Updated MaxOfThreeNums program for better readability

--- chapter1/MaxOfThreeNums.java
+++ chapter1/MaxOfThreeNums.java
@@ -3,24 +3,17 @@ import java.util.Scanner;
 public class MaxOfThreeNums {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter three numbers: ");
+        System.out.print("Enter three numbers: ");
         int num1 = input.nextInt(), num2 = input.nextInt(), num3 = input.nextInt();
 
-        if(num1 > num2 ){
-            if(num1 > num3)
-              System.out.println(num1 + " is the greatest");
-
-            else if(num2 > num3)
-             System.out.println(num2 + " is greatest");
-
-            else System.out.println(num3 + " is greatest");
+        if (num1 >= num2 && num1 >= num3) {
+            System.out.println(num1 + " is the greatest");
+        } else if (num2 >= num1 && num2 >= num3) {
+            System.out.println(num2 + " is the greatest");
+        } else {
+            System.out.println(num3 + " is the greatest");
         }
-        else{
-            if(num2 > num3)
-             System.out.println(num2 + " is greatest");
 
-            else System.out.println(num3 + " is greatest");
-        }
         input.close();
     }
 }
